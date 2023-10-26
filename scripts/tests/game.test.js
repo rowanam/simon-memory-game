9915c07483d7ceedf64b473e8c69fc205e7f3743
rowanam <rowanamaas@gmail.com>
2023-10-26 18:17:51 +0000

    Add choice button ids to game object

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+const { expect } = require("@jest/globals");
 const {
     game
 } = require("../game");
@@ -26,5 +27,8 @@ describe("game object contains correct keys", () => {
     });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
